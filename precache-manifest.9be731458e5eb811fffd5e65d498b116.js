78bee1f53e2c6c7f47396706d38a934388d2d99b
priyank satasiya <priyanksatasiya81099@gmail.com>
2019-06-23 14:26:14 +0530

    Updates

---
+++ precache-manifest.9be731458e5eb811fffd5e65d498b116.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80984461d30e8afec1da8bcb65f0257e",
+    "url": "/avatar/index.html"
+  },
+  {
+    "revision": "b7a5eac1bffeb76417d3",
+    "url": "/avatar/static/css/main.7d5aaedd.chunk.css"
+  },
+  {
+    "revision": "961a9cd9842d989b1c25",
+    "url": "/avatar/static/js/2.e96ba575.chunk.js"
+  },
+  {
+    "revision": "b7a5eac1bffeb76417d3",
+    "url": "/avatar/static/js/main.b08550d7.chunk.js"
+  },
+  {
+    "revision": "70be0630061b7b69076f",
+    "url": "/avatar/static/js/runtime~main.07137c7e.js"
+  }
+]);
